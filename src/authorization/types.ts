0802cf95b97e67a0c2fdfd3a841e15bd7f957fd2
Erwin Flores <ramiromarca6@gmail.com>
2021-09-29 23:06:18 -0400

    init WebIniaf Server

---
+++ src/authorization/types.ts
@@ -0,0 +1,73 @@
+import {UserProfile} from '@loopback/security';
+import {SchemaObject} from 'openapi3-ts';
+import {PermissionKey} from './permission-key';
+export interface UserPermissionsFn {
+  (
+    userPermissions: PermissionKey[],
+    requiredPermissions: RequiredPermissions,
+  ): boolean;
+}
+
+export interface AuthUser extends UserProfile {
+  id?: string;
+  name: string;
+  email: string;
+  permissions: PermissionKey[];
+}
+
+export interface RequiredPermissions {
+  required: PermissionKey[];
+}
+
+export const UserProfileSchema = {
+  type: 'object',
+  required: ['email', 'password', 'name'],
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email',
+    },
+    password: {
+      type: 'string',
+      minLength: 8,
+    },
+    name: {type: 'string'},
+  },
+};
+
+export const UserRequestBody = {
+  description: 'The input of create user function',
+  required: true,
+  content: {
+    'application/json': {schema: UserProfileSchema},
+  },
+};
+
+export interface AuthCredential {
+  email: string;
+  password: string;
+  permissions: PermissionKey[];
+}
+
+export const CredentialsSchema: SchemaObject = {
+  type: 'object',
+  required: ['email', 'password'],
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email',
+    },
+    password: {
+      type: 'string',
+      minLength: 8,
+    },
+  },
+};
+
+export const CredentialsRequestBody = {
+  description: 'The input of login function',
+  required: true,
+  content: {
+    'application/json': {schema: CredentialsSchema},
+  },
+};
